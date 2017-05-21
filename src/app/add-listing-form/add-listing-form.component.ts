7be0840be918516a52058b5ecb71258dacfd15b7
Jungmin <jungmin.yjm@gmail.com>
2017-05-21 18:32:44 -0400

    generate add-listing-form component

---
+++ src/app/add-listing-form/add-listing-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-listing-form',
+  templateUrl: './add-listing-form.component.html',
+  styleUrls: ['./add-listing-form.component.css']
+})
+export class AddListingFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
